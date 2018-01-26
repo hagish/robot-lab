98ee77c4914ad680b56cb0f5f0ae26cdb7bfd377
David Hubber <david.hubber@googlemail.com>
2018-01-26 20:54:18 +0100

    Basic Agent script added to respond to commands triggered by Signal object.  Also added Signal script (which is POD).

--- Assets/Scripts/Agent.cs
+++ Assets/Scripts/Agent.cs
@@ -4,13 +4,90 @@ using UnityEngine;
 
 public class Agent : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	const float commandDuration = 1.0f;
+	const float movementSpeed = 0.05f;
+
+	private int currentSignalGroupId;
+	private float timeLastSignal;
+	private Vector3 dir;
+
+	public int playerGroupId;
+
+
+	void Awake()
+	{
+		dir = new Vector3 (0.0f, 0.0f, 0.0f);
+		timeLastSignal = -100.0f;
+		currentSignalGroupId = -1;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+
+	void Start ()
+	{
 	}
+
+
+	void Update ()
+	{
+		float currentTime = Time.time;
+
+		if (currentTime - timeLastSignal < commandDuration) {
+			transform.parent.transform.position += movementSpeed * dir;
+			Debug.Log ("Moving agent; dir : " + dir);
+		}
+	}
+
+
+	void ProcessCommand(string command)
+	{
+		switch (command)
+		{
+		case "Left":
+			Debug.Log ("Agent moving left");
+			timeLastSignal = Time.time;
+			dir = new Vector3 (-1.0f, 0.0f, 0.0f);
+			break;
+
+		case "Right":
+			Debug.Log ("Agent moving right");
+			timeLastSignal = Time.time;
+			dir = new Vector3 (1.0f, 0.0f, 0.0f);
+			break;
+
+		case "Down":
+			Debug.Log ("Agent moving down");
+			timeLastSignal = Time.time;
+			dir = new Vector3 (0.0f, 0.0f, -1.0f);
+			break;
+
+		case "Up":
+			Debug.Log ("Agent moving up");
+			timeLastSignal = Time.time;
+			dir = new Vector3 (0.0f, 0.0f, 1.0f);
+			break;
+
+		default:
+			Debug.Log("Unrecognised command : " + command);
+			break;
+		}
+	}
+
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "signal") {
+
+			Debug.Log("Detected signal particle");
+			Signal signal = other.gameObject.GetComponent<Signal> ();
+
+			// If signal originates from the wrong player(s), then ignore
+			if (signal.GetPlayerOriginId () != playerGroupId) return;
+
+			// If signal came from previously received signal, then also ignore
+			if (signal.GetSignalGroupId() == currentSignalGroupId) return;
+
+			ProcessCommand (signal.GetCommand ());
+		}
+	}
+
 }
---
+++ Assets/Scripts/Signal.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Signal : MonoBehaviour
+{
+	private string command;
+	private int playerOriginId;
+	private int signalGroupId;
+
+	public void SetCommand(string _command, int _playerOriginId, int _signalGroupId)
+	{
+		command        = _command;
+		playerOriginId = _playerOriginId;
+		signalGroupId  = _signalGroupId;
+	}
+
+	public string GetCommand() {return command;}
+	public int GetPlayerOriginId() {return playerOriginId;}
+	public int GetSignalGroupId() {return signalGroupId;}
+}
