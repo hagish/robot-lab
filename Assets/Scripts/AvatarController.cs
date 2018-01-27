bf7d287815ddda90ecf8ad45ed681cbb086f5ec9
lead001 <v.lead001@gmail.com>
2018-01-27 03:30:13 +0100

    Added controls

--- Assets/Scripts/AvatarController.cs
+++ Assets/Scripts/AvatarController.cs
@@ -10,6 +10,8 @@ public class AvatarController : MonoBehaviour
     public float jumpSpeed = 8.0F;
     public float gravity = 20.0F;
     private Vector3 moveDirection = Vector3.zero;
+
+    private Vector3 faceDirection = Vector3.fwd;
     public XboxController XboxController;
 
     public Aim Aim;
@@ -31,50 +33,64 @@ public class AvatarController : MonoBehaviour
         CharacterController controller = GetComponent<CharacterController>();
         moveDirection = Vector3.zero;
 
-       // if (controller.isGrounded)
+    
+        float dx = XCI.GetAxis(XboxAxis.LeftStickX, XboxController);
+        float dy = XCI.GetAxis(XboxAxis.LeftStickY, XboxController);
+
+        moveDirection = new Vector3(dx, 0, dy);
+        if(moveDirection.magnitude > inputThreshold)
         {
-
-            float dx = XCI.GetAxis(XboxAxis.LeftStickX, XboxController);
-            float dy = XCI.GetAxis(XboxAxis.LeftStickY, XboxController);
-
-            moveDirection = new Vector3(dx, 0, dy);
-            if(moveDirection.magnitude > inputThreshold)
-            {
-                lastMoveDirection = moveDirection;
-                moveDirection = transform.TransformDirection(moveDirection);
-                moveDirection *= speed;
-                controller.Move(moveDirection * Time.deltaTime);
-
-             }
+            lastMoveDirection = moveDirection;
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+            controller.Move(moveDirection * Time.deltaTime);
         }
+        SetRotation();
+        CallTrigger(faceDirection);    
+    }
 
+    public void CallTrigger(Vector3 direction)
+    {
+       if (triggerAction != null) 
+       {
+            if (XCI.GetButtonDown(XboxButton.LeftBumper, XboxController))
+            {
+                triggerAction(direction, "Up");
+			}
 
+            if (XCI.GetButtonDown(XboxButton.RightBumper, XboxController))
+            {
+                triggerAction(direction, "Down");
+            } 
+           if (XCI.GetButtonDown(XboxButton.LeftStick, XboxController))
+            {
+                triggerAction(direction, "Left");
+            }
+            if (XCI.GetButtonDown(XboxButton.RightStick, XboxController))
+            {
+                triggerAction(direction, "Right");
+            }
+            if (XCI.GetAxis(XboxAxis.LeftTrigger, XboxController) > 0.6f)
+            {
+                triggerAction(direction, "Move");
+            }
+            if (XCI.GetAxis(XboxAxis.RightTrigger, XboxController) > 0.6f)
+            {
+                triggerAction(direction, "Block");
+            }
+    	}
+    }
+    public void SetRotation()
+    {
         float dxR = XCI.GetAxis(XboxAxis.RightStickX, XboxController);
         float dyR = XCI.GetAxis(XboxAxis.RightStickY, XboxController);
 
         var direction = new Vector3(dxR, 0, dyR);
-        if (Aim != null) Aim.SetDirection(direction);
 
-       if (triggerAction != null) {
-            if (Input.GetKey(KeyCode.I)) {
-                triggerAction(direction, "Up");
-			}
-            if (Input.GetKey(KeyCode.K)) {
-                triggerAction(direction, "Down");
-            }
-            if (Input.GetKey(KeyCode.J)) {
-                triggerAction(direction, "Left");
-            }
-            if (Input.GetKey(KeyCode.L)) {
-                triggerAction(direction, "Right");
-            }
-            if (Input.GetKey(KeyCode.Space)) {
-                triggerAction(direction, "Move");
-            }
-            if (Input.GetKey(KeyCode.B)) {
-                triggerAction(direction, "Block");
-            }
-    	}
-            
+        if(direction.magnitude > 0.2f )
+            faceDirection = direction;
+
+        if (Aim != null) Aim.SetDirection(faceDirection);
+
     }
 }
