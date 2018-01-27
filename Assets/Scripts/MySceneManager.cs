0f94cd9982831d86a8cd2f9ad2b5120acf9c042c
Sebastian Dorda <hagish@schattenkind.net>
2018-01-27 19:17:24 +0100

    menu

---
+++ Assets/Scripts/MySceneManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MySceneManager : MonoBehaviour {
+    public static MySceneManager Instance;
+
+    private void Awake() {
+        if (Instance != null && Instance != this) {
+            GameObject.Destroy(gameObject);
+        } else {
+            Instance = this;
+        }
+
+        GameObject.DontDestroyOnLoad(gameObject);
+    }
+
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            SceneManager.LoadScene("_menu", LoadSceneMode.Single);
+        } else if (Input.GetKeyDown(KeyCode.F1)) {
+            SceneManager.LoadScene("_level0", LoadSceneMode.Single);
+        } else if (Input.GetKeyDown(KeyCode.F2)) {
+            SceneManager.LoadScene("_level1", LoadSceneMode.Single);
+        } else if (Input.GetKeyDown(KeyCode.F3)) {
+            SceneManager.LoadScene("_level2", LoadSceneMode.Single);
+        }
+    }
+}
