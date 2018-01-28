e6d801ad40da1db5b2065332350ad3b9e3566d60
Sebastian Dorda <hagish@schattenkind.net>
2018-01-28 15:44:22 +0100

    info text

---
+++ Assets/Scripts/UIGoalInfo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIGoalInfo : MonoBehaviour {
+
+    public AnimationCurve Curve;
+    public float Delay;
+    public float FadeTime;
+
+	IEnumerator Start () {
+        yield return new WaitForSeconds(Delay);
+
+        var text = GetComponent<Text>();
+
+        var c = text.color;
+
+        var t0 = Time.time;
+        while (Time.time < t0 + FadeTime) {
+            yield return null;
+            var f = (Time.time - t0) / FadeTime;
+            c.a = Curve.Evaluate(f);
+            text.color = c;
+        }
+        gameObject.SetActive(false);
+    }
+}
